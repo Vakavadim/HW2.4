f5c462ac9651a9936e9f97cd3104f522813e9b72
Vadim Gamzaev <Vadosbox@gmail.com>
2019-09-29 21:47:07 +0300

    Initial Commit

---
+++ HW2.4/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HW2.4
+//
+//  Created by Вадим Гамзаев on 29/09/2019.
+//  Copyright © 2019 Вадим Гамзаев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
